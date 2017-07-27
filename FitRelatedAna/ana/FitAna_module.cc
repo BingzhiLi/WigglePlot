8f3d6cce0bec36e2db1f4bb13f96a530145c8b46
BingzhiLi <2570136643@qq.com>
2017-07-27 09:26:37 +0800

    update code

--- FitRelatedAna/ana/FitAna_module.cc
+++ FitRelatedAna/ana/FitAna_module.cc
@@ -54,9 +54,6 @@ private:
   int runNumber_;
   int subrunNumber_;
 
-
-
-
   int nCalos_; // calo number
   int nXtals_; // crystal number
   double wiggleThreshold_; // official Energy threshold = 1860MeV * 0.75pe/MeV = 1395
@@ -83,6 +80,17 @@ private:
   TH1D *threshold20;
   TH1D *threshold21;
   TH1D *threshold22;
+  //////////////energy slices histogram
+  TH1D *slice1617;
+  TH1D *slice1718;
+  TH1D *slice1819;
+  TH1D *slice1920;
+  TH1D *slice2021;
+  TH1D *slice2122;
+  TH1D *slice1618;
+  TH1D *slice1820;
+  TH1D *slice2022;
+  
 };
 
 
@@ -123,6 +131,16 @@ gm2calo::FitAna::FitAna(fhicl::ParameterSet const & p)
   threshold21 = allCaloDir.make<TH1D>("wigglethreshold2100MeV","T Method with threshold 2.1GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
   threshold22 = allCaloDir.make<TH1D>("wigglethreshold2200MeV","T Method with threshold 2.2GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
 
+  slice1617 = allCaloDir.make<TH1D>("energy_slice_1600_1700MeV","T Method with energy slice 1.6 to 1.7 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+  slice1718 = allCaloDir.make<TH1D>("energy_slice_1700_1800MeV","T Method with energy slice 1.7 to 1.8 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+  slice1819 = allCaloDir.make<TH1D>("energy_slice_1800_1900MeV","T Method with energy slice 1.8 to 1.9 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+  slice1920 = allCaloDir.make<TH1D>("energy_slice_1900_2000MeV","T Method with energy slice 1.9 to 2.0 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+  slice2021 = allCaloDir.make<TH1D>("energy_slice_2000_2100MeV","T Method with energy slice 2.0 to 2.1 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+  slice2122 = allCaloDir.make<TH1D>("energy_slice_2100_2200MeV","T Method with energy slice 2.1 to 2.2 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+
+  slice1618 = allCaloDir.make<TH1D>("energy_slice_1600_1800MeV","T Method with energy slice 1.6 to 1.8 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+  slice1820 = allCaloDir.make<TH1D>("energy_slice_1800_2000MeV","T Method with energy slice 1.8 to 2.0 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
+  slice2022 = allCaloDir.make<TH1D>("energy_slice_2000_2200MeV","T Method with energy slice 2.0 to 2.2 GeV, all calos; time [clock ticks]; N;", 4700, 0, 560000);
 }
 
 void gm2calo::FitAna::analyze(art::Event const & e)
@@ -149,17 +167,27 @@ void gm2calo::FitAna::analyze(art::Event const & e)
       caloWiggles_[caloIndex]->Fill(cluster.time);
     } 
 
-    if (cluster.energy > th16) {threshold16->Fill(cluster.time);}
-    if (cluster.energy > th17) {threshold17->Fill(cluster.time);}
-    if (cluster.energy > th18) {threshold18->Fill(cluster.time);}
-    if (cluster.energy > th19) {threshold19->Fill(cluster.time);}
-    if (cluster.energy > th20) {threshold20->Fill(cluster.time);}
-    if (cluster.energy > th21) {threshold21->Fill(cluster.time);}
-    if (cluster.energy > th22) {threshold22->Fill(cluster.time);}
+    //energy threshold above 1.6 1.7 1.8 1.9 2.0 2.1 2.2 GeV
+    if (cluster.energy > th16) { threshold16->Fill(cluster.time); }
+    if (cluster.energy > th17) { threshold17->Fill(cluster.time); }
+    if (cluster.energy > th18) { threshold18->Fill(cluster.time); }
+    if (cluster.energy > th19) { threshold19->Fill(cluster.time); }
+    if (cluster.energy > th20) { threshold20->Fill(cluster.time); } 
+    if (cluster.energy > th21) { threshold21->Fill(cluster.time); }
+    if (cluster.energy > th22) { threshold22->Fill(cluster.time); }
     
+    //energy slices 1.6-1.7 1.7-1.8 1.8-1.9 1.9-2.0 2.0-2.1 2.1-2.2 
+    if (cluster.energy > th16 && cluster.energy < th17) { slice1617->Fill(cluster.time); }
+    if (cluster.energy > th17 && cluster.energy < th18) { slice1718->Fill(cluster.time); }
+    if (cluster.energy > th18 && cluster.energy < th19) { slice1819->Fill(cluster.time); }
+    if (cluster.energy > th19 && cluster.energy < th20) { slice1920->Fill(cluster.time); }
+    if (cluster.energy > th20 && cluster.energy < th21) { slice2021->Fill(cluster.time); }
+    if (cluster.energy > th21 && cluster.energy < th22) { slice2122->Fill(cluster.time); }
 
-
-
+    //energy slices 1.6-1.8 1.8-2.0 2.0-2.2
+    if (cluster.energy > th16 && cluster.energy < th18) { slice1618->Fill(cluster.time); }
+    if (cluster.energy > th18 && cluster.energy < th20) { slice1820->Fill(cluster.time); }
+    if (cluster.energy > th20 && cluster.energy < th22) { slice2022->Fill(cluster.time); }    
 
   });
 
